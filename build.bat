88ce482d1504ae8eb08653fe1d04912f26972b24
Oraoto <melegant2@gmail.com>
2018-08-05 14:54:30 +0800

    Build against oraoto/visualDL

--- build.bat
+++ build.bat
@@ -1,13 +1,22 @@
+REM Setup MSVC
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-git clone https://github.com/PaddlePaddle/VisualDL.git
+REM Clone VisualDL
+git clone https://github.com/oraoto/VisualDL.git
 cd VisualDL
-git checkout develop
+git checkout windows-v1.1.0
 
+REM Setup environment variables
 set "PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%"
 set "LIB=%PYTHON%\\libs;%LIB%"
 
+REM Install python dependencies
 python -m pip install --upgrade pip
+pip install wheel
 pip install -r requirements.txt
+
+Rem Python 3.6.6 in AppVeyor has issue with numpy 1.14
 pip install numpy==1.13 -U
+
+Rem Build
 python setup.py bdist_wheel
